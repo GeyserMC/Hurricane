b3fc1afc2600624974456cf14d606ff5fae24dce
TBYT <TBYTBiz@googlegroups.com>
2022-11-18 23:27:23 -0800

    development

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -1,11 +1,12 @@
 package net.camotoy.geyserhacks;
 
+import java.util.List;
+
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.TradeSelectEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.plugin.Plugin;
 
 public final class SweepingEdgeFix implements Listener 
@@ -21,19 +22,20 @@ public final class SweepingEdgeFix implements Listener
     @EventHandler
     public void onVillagerInteract(final TradeSelectEvent event) 
     {
-    	for (MerchantRecipe recipe : event.getMerchant().getRecipes())
+    	for (int i = 0; i < event.getMerchant().getRecipes().size(); i++) 
     	{
-    		for (ItemStack s : recipe.getIngredients())
+    		List<ItemStack> Items = event.getMerchant().getRecipe(i).getIngredients();
+    		for(int x=0; x < Items.size(); x++)
     		{
-    			if(s.containsEnchantment(Enchantment.SWEEPING_EDGE))
+    			event.getMerchant().getTrader().getServer().broadcastMessage("Item: "+Items.get(x));
+    			if(Items.get(x).containsEnchantment(Enchantment.SWEEPING_EDGE))
     			{
-    				s.addEnchantment(Enchantment.DURABILITY, 1);
-    				event.getMerchant().getTrader().getServer().broadcastMessage("ItemStack: "+s);
+    				if(!Items.get(x).containsEnchantment(Enchantment.DURABILITY))
+    					Items.get(x).addUnsafeEnchantment(Enchantment.DURABILITY, 1);
     				event.getMerchant().getTrader().getServer().broadcastMessage("Fixed enchantment?");
     			}
-	    		
     		}
-		}
+    	}
         
     }
 }
