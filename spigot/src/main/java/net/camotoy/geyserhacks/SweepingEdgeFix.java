281cc3445ea8dc82e24900c90c6279c6ac6f8575
TBYT <TBYTBiz@googlegroups.com>
2022-11-18 22:03:55 -0800

    development

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -3,6 +3,7 @@ package net.camotoy.geyserhacks;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.TradeSelectEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
 import org.bukkit.plugin.Plugin;
 
@@ -19,8 +20,15 @@ public final class SweepingEdgeFix implements Listener
     @EventHandler
     public void onVillagerInteract(final TradeSelectEvent event) 
     {
-    	for (MerchantRecipe recipe : event.getMerchant().getRecipes()) {
-			event.getMerchant().getTrader().sendMessage("Recipe: "+recipe);
+    	for (MerchantRecipe recipe : event.getMerchant().getRecipes())
+    	{
+    		//event.getMerchant().getTrader().sendMessage("Ingredients for each recipes: "+recipe.getIngredients());
+    		event.getMerchant().getTrader().getServer().broadcastMessage("Ingredients for each recipes: "+recipe.getIngredients());
+    		for (ItemStack s : recipe.getIngredients())
+    		{
+				//event.getMerchant().getTrader().sendMessage("ItemStack: "+s);
+	    		event.getMerchant().getTrader().getServer().broadcastMessage("ItemStack: "+s);
+    		}
 		}
         
     }
