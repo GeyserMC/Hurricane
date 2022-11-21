65572718100f5d88e5e5c3db2f0fe4fda47b9ecb
TBYT <TBYTBiz@googlegroups.com>
2022-11-20 20:42:02 -0800

    development

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -5,6 +5,7 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
@@ -13,41 +14,47 @@ import org.bukkit.plugin.Plugin;
 public final class SweepingEdgeFix implements Listener 
 {
 
-    public SweepingEdgeFix(Plugin plugin) {
-        
+	private final NMSProvider provider;
+	private final Plugin plugin;
+	
+    public SweepingEdgeFix(Plugin plugin, final NMSProvider provider) {
+        this.plugin = plugin;
+        this.provider = provider;
     }
     
     @EventHandler
     public void findEnchant(InventoryOpenEvent e) 
     {
-    	e.getPlayer().sendMessage("inventory detected");
-    	PlayerInventory inv = e.getPlayer().getInventory();
-        for(int i = 0; i < inv.getSize()-1; i++)
-        {
-        	 ItemStack item = inv.getItem(i);
-        	 e.getPlayer().sendMessage(i+"");
-             if(item.getType().equals(Material.BOOK) 
-            		 || item.getType().equals(Material.DIAMOND_SWORD) 
-            		 || item.getType().equals(Material.NETHERITE_SWORD)
-            		 || item.getType().equals(Material.IRON_SWORD)
-            		 || item.getType().equals(Material.GOLDEN_SWORD)
-            		 || item.getType().equals(Material.STONE_SWORD)
-            		 || item.getType().equals(Material.WOODEN_SWORD))
-             {
-            	 EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
-
-                 if (meta.hasStoredEnchant(Enchantment.SWEEPING_EDGE)) 
-                 {
-                	 e.getPlayer().sendMessage("detected sweeping edge");
-                	 if (!meta.hasStoredEnchant(Enchantment.DURABILITY)) 
-                     {
-                		 e.getPlayer().sendMessage("changed book");
-	                     meta.addStoredEnchant(Enchantment.DURABILITY, 1, false);
-	                     meta.setDisplayName("Sweeping Edge "+item.getItemMeta().getEnchantLevel(Enchantment.SWEEPING_EDGE)+" "+item.getType().name());
-	                     item.setItemMeta(meta);
-                     }
-                 }
-             }
-        }
+    	if (e.getInventory().getType() == InventoryType.PLAYER)
+    	{
+	    	e.getPlayer().sendMessage("player inventory detected");
+	    	PlayerInventory inv = e.getPlayer().getInventory();
+	        for(int i = 0; i < inv.getSize()-1; i++)
+	        {
+	        	 ItemStack item = inv.getItem(i);
+	        	 e.getPlayer().sendMessage(i+"");
+	             if(item.getType().equals(Material.ENCHANTED_BOOK) 
+	            		 || item.getType().equals(Material.DIAMOND_SWORD) 
+	            		 || item.getType().equals(Material.NETHERITE_SWORD)
+	            		 || item.getType().equals(Material.IRON_SWORD)
+	            		 || item.getType().equals(Material.GOLDEN_SWORD)
+	            		 || item.getType().equals(Material.STONE_SWORD)
+	            		 || item.getType().equals(Material.WOODEN_SWORD))
+	             {
+	            	 EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
+	                 if (meta.hasStoredEnchant(Enchantment.SWEEPING_EDGE)) 
+	                 {
+	                	 e.getPlayer().sendMessage("detected sweeping edge");
+	                	 if (!meta.hasStoredEnchant(Enchantment.DURABILITY)) 
+	                     {
+	                		 e.getPlayer().sendMessage("changed book");
+		                     meta.addStoredEnchant(Enchantment.DURABILITY, 1, false);
+		                     meta.setDisplayName("Sweeping Edge "+item.getItemMeta().getEnchantLevel(Enchantment.SWEEPING_EDGE)+" "+item.getType().name());
+		                     item.setItemMeta(meta);
+	                     }
+	                 }
+	             }
+	        }
+    	}
     }
 }
