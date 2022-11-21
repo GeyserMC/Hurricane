5239dfbf9958f74966b358fe27e1ec8a45cd36fb
TBYT <TBYTBiz@googlegroups.com>
2022-11-20 20:55:24 -0800

    development

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -10,6 +10,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.plugin.Plugin;
+import net.camotoy.geyserhacks.NMSProvider;
 
 public final class SweepingEdgeFix implements Listener 
 {
