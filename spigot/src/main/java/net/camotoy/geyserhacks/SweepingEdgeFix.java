e1dd2c1010518dfad73d3350db5fb51ff8261ae9
TBYT <TBYTBiz@googlegroups.com>
2022-11-21 19:06:01 -0800

    development

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -2,13 +2,18 @@ package net.camotoy.geyserhacks;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+import net.kyori.adventure.text.Component;
+
+import org.bukkit.event.inventory.InventoryType;
 
 public final class SweepingEdgeFix implements Listener 
 {
@@ -19,35 +24,36 @@ public final class SweepingEdgeFix implements Listener
     }
     
     @EventHandler
-    public void findEnchant(PlayerInteractEvent e) 
+    public void findEnchant(InventoryClickEvent e) 
     {
-	    	PlayerInventory inv = e.getPlayer().getInventory();
-	        for(int i = 0; i < inv.getSize()-1; i++)
-	        {
-	        	 ItemStack item = inv.getItem(i);
-	        	 e.getPlayer().sendMessage(i+"");
-	             if(item.getType().equals(Material.ENCHANTED_BOOK) 
-	            		 || item.getType().equals(Material.DIAMOND_SWORD) 
-	            		 || item.getType().equals(Material.NETHERITE_SWORD)
-	            		 || item.getType().equals(Material.IRON_SWORD)
-	            		 || item.getType().equals(Material.GOLDEN_SWORD)
-	            		 || item.getType().equals(Material.STONE_SWORD)
-	            		 || item.getType().equals(Material.WOODEN_SWORD))
-	             {
-	            	 EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
-	                 if (meta.hasStoredEnchant(Enchantment.SWEEPING_EDGE)) 
-	                 {
-	                	 e.getPlayer().sendMessage("detected sweeping edge");
-	                	 if (!meta.hasStoredEnchant(Enchantment.DURABILITY)) 
-	                     {
-	                		 e.getPlayer().sendMessage("changed book");
-		                     meta.addStoredEnchant(Enchantment.DURABILITY, 1, false);
-		                     meta.setDisplayName("Sweeping Edge "+item.getItemMeta().getEnchantLevel(Enchantment.SWEEPING_EDGE)+" "+item.getType().name());
-		                     item.setItemMeta(meta);
-	                     }
-	                 }
-	             }
-	        }
-    	
+		Player player = (Player) e.getWhoClicked();
+		if (e.getClickedInventory().getType() == InventoryType.PLAYER) 
+		{
+			ItemStack item = e.getCurrentItem();
+			if (item.getType().equals(Material.ENCHANTED_BOOK) || item.getType().equals(Material.DIAMOND_SWORD)
+					|| item.getType().equals(Material.NETHERITE_SWORD) || item.getType().equals(Material.IRON_SWORD)
+					|| item.getType().equals(Material.GOLDEN_SWORD) || item.getType().equals(Material.STONE_SWORD)
+					|| item.getType().equals(Material.WOODEN_SWORD)) 
+			{
+				ItemMeta meta = item.getItemMeta();
+				if (meta.hasEnchant(Enchantment.SWEEPING_EDGE)) 
+				{
+					player.sendMessage("detected sweeping edge");
+					int sweepingLevel = meta.getEnchantLevel(Enchantment.SWEEPING_EDGE);
+					String displayName = item.getType().name();
+					if(meta.hasDisplayName()) { displayName = meta.displayName().toString(); }
+					meta.setDisplayName("Sweeping Edge " + sweepingLevel+ " " + displayName);
+					item.setItemMeta(meta);
+					if (!meta.hasEnchant(Enchantment.DURABILITY)) 
+					{
+						item.addEnchantment(Enchantment.DURABILITY, 1);
+						player.sendMessage("added unbreaking1");
+					}
+					player.sendMessage("changed item");
+					e.setCurrentItem(item);
+				}
+			}
+		}
+
     }
 }
