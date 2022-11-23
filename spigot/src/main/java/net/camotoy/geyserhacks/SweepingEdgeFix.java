b7d0ad4cb98d028cfe08e7e6878cde7c998f0fab
TBYT <TBYTBiz@googlegroups.com>
2022-11-22 17:46:37 -0800

    ctrl shift f + ctrl i

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -13,64 +13,58 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.event.inventory.InventoryType;
 import org.geysermc.floodgate.api.FloodgateApi;
 
-public final class SweepingEdgeFix implements Listener 
-{
+public final class SweepingEdgeFix implements Listener {
 	private final Plugin plugin;
-	
-    public SweepingEdgeFix(Plugin plugin) {
-        this.plugin = plugin;
-    }
 
-    /*
-     * TBYT adds Sweeping Edge fix.
-     * This adds unbreaking 1 to sweeping edge items if they only have the 1 enchant(sweeping edge)
-     * If has sweeping edge and another enchant, or after applying unbreaking fix, wil update item name to sweeping edge and the enchantment level.
-     */
-    @EventHandler
-    public void findEnchant(InventoryClickEvent event) 
-    {
+	public SweepingEdgeFix(Plugin plugin) {
+		this.plugin = plugin;
+	}
+
+	/*
+	 * TBYT adds Sweeping Edge fix. This adds unbreaking 1 to sweeping edge items if
+	 * they only have the 1 enchant(sweeping edge) If has sweeping edge and another
+	 * enchant, or after applying unbreaking fix, wil update item name to sweeping
+	 * edge and the enchantment level.
+	 */
+	@EventHandler
+	public void findEnchant(InventoryClickEvent event) {
 		Player player = (Player) event.getWhoClicked();
-		//Checking for floodgate/geyser player.
-        if (FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) 
-        {
-        	//Inventory becomes null for some reason after player clicks on item then drops it out their inventory.
-			if(event.getClickedInventory()!=null)
-			{
-				if (event.getClickedInventory().getType() == InventoryType.PLAYER) 
-				{
+		// Checking for floodgate/geyser player.
+		if (FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) {
+			// Inventory becomes null after player clicks on item then drops it out their
+			// inventory.
+			if (event.getClickedInventory() != null) {
+				if (event.getClickedInventory().getType() == InventoryType.PLAYER) {
 					ItemStack item = event.getCurrentItem();
-					if(item!=null) //rare case this equals null
+					if (item != null) // rare case this equals null
 					{
 						if (item.getType().equals(Material.DIAMOND_SWORD)
-								|| item.getType().equals(Material.NETHERITE_SWORD) || item.getType().equals(Material.IRON_SWORD)
-								|| item.getType().equals(Material.GOLDEN_SWORD) || item.getType().equals(Material.STONE_SWORD)
-								|| item.getType().equals(Material.WOODEN_SWORD)) 
-						{
+								|| item.getType().equals(Material.NETHERITE_SWORD)
+								|| item.getType().equals(Material.IRON_SWORD)
+								|| item.getType().equals(Material.GOLDEN_SWORD)
+								|| item.getType().equals(Material.STONE_SWORD)
+								|| item.getType().equals(Material.WOODEN_SWORD)) {
 							ItemMeta meta = item.getItemMeta();
-							if (meta.hasEnchant(Enchantment.SWEEPING_EDGE)) 
-							{
+							if (meta.hasEnchant(Enchantment.SWEEPING_EDGE)) {
 								int sweepingLevel = meta.getEnchantLevel(Enchantment.SWEEPING_EDGE);
 								String displayName = item.getType().name();
 								int charAt = displayName.indexOf("_");
 								meta.setDisplayName("Sweeping Edge " + sweepingLevel);
-								if(meta.getEnchants().size()==1)
-								{
+								if (meta.getEnchants().size() == 1) {
 									meta.addEnchant(Enchantment.DURABILITY, 1, false);
-									player.sendMessage("Sweeping Edge Fixed on "+displayName.substring(0,1).toUpperCase()+displayName.substring(1, charAt).toLowerCase()+" Sword.");
+									player.sendMessage(
+											"Sweeping Edge Fixed on " + displayName.substring(0, 1).toUpperCase()
+											+ displayName.substring(1, charAt).toLowerCase() + " Sword.");
 								}
 								item.setItemMeta(meta);
 								event.setCurrentItem(item);
 							}
-						}
-						else if(item.getType().equals(Material.ENCHANTED_BOOK))
-						{
+						} else if (item.getType().equals(Material.ENCHANTED_BOOK)) {
 							EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
-							if (meta.hasStoredEnchant(Enchantment.SWEEPING_EDGE)) 
-							{
+							if (meta.hasStoredEnchant(Enchantment.SWEEPING_EDGE)) {
 								int sweepingLevel = meta.getStoredEnchantLevel(Enchantment.SWEEPING_EDGE);
 								meta.setDisplayName("Sweeping Edge " + sweepingLevel);
-								if(meta.getStoredEnchants().size()==1)
-								{
+								if (meta.getStoredEnchants().size() == 1) {
 									meta.addStoredEnchant(Enchantment.DURABILITY, 1, false);
 									player.sendMessage("Sweeping Edge Fixed on Enchanted Book.");
 								}
@@ -81,6 +75,6 @@ public final class SweepingEdgeFix implements Listener
 					}
 				}
 			}
-        }
-    }
+		}
+	}
 }
