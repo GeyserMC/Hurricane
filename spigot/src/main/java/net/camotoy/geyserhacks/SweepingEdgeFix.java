6b9faaffceb4b81298c9a0b0d95c481165a6f415
TBYT <TBYTBiz@googlegroups.com>
2022-12-01 18:20:37 -0800

    development

--- spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
+++ spigot/src/main/java/net/camotoy/geyserhacks/SweepingEdgeFix.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -103,4 +104,10 @@ public final class SweepingEdgeFix implements Listener {
 	        }
 		}
     }
+	
+	@EventHandler
+	public void anvilPrematureExit(InventoryCloseEvent event) 
+	{
+		event.getPlayer().sendMessage(event.getReason()+"");
+	}
 }
